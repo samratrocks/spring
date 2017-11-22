1cf83b9dfe88d424a37f6f590230d851bdb6f7f9
Jonas Luebbers <jluebbers@squarespace.com>
2017-11-22 12:00:38 -0500

    Click to set panel position

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,13 @@
 export default class Main {
 	constructor() {
-		const panel = document.getElementsByClassName('spring-panel')
+		this.panel = document.getElementsByClassName('spring-panel')[0]
+
+		document.addEventListener('click', this.handleClick.bind(this))
+	}
+
+	handleClick(event) {
+		const x = event.clientX / window.innerWidth * 100
+		this.panel.style.transform = `translate3d(${x}%, 0, 0)`
 	}
 }
 
