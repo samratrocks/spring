df179df75cd3d4bfb3e1ce2fec827704b5df04dd
Jonas Luebbers <jluebbers@squarespace.com>
2017-11-22 12:02:30 -0500

    Add animation loop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,17 @@
+import AnimationLoop from '@nonphoto/animation-loop'
+
 export default class Main {
 	constructor() {
 		this.panel = document.getElementsByClassName('spring-panel')[0]
 
+		this.animationLoop = new AnimationLoop(this.handleAnimationFrame.bind(this))
+		this.animationLoop.start()
+
 		document.addEventListener('click', this.handleClick.bind(this))
+	}
+
+	handleAnimationFrame() {
+
 	}
 
 	handleClick(event) {
