fb5898abab94e36ab7317172efd9d390913d5ee6
Jonas Luebbers <jluebbers@squarespace.com>
2017-11-22 11:56:14 -0500

    Add basic layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,9 @@
 export default class Main {
 	constructor() {
-
+		const panel = document.getElementsByClassName('spring-panel')
 	}
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+	const main = new Main()
+})
