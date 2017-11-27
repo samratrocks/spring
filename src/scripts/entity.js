48ada2b30497170e713721b7b52a94d4396d9022
Jonas Luebbers <jluebbers@squarespace.com>
2017-11-27 10:55:07 -0500

    Add mass property to entities

--- src/scripts/entity.js
+++ src/scripts/entity.js
@@ -1,13 +1,16 @@
 import Vector from '@nonphoto/vector'
 
 export default class Entity {
-	constructor(x, y) {
+	constructor(x = 0, y = 0, mass = 1) {
 		this.position = new Vector(x, y)
-		this.velocity = new Vector()
+        this.velocity = new Vector()
+        this.mass = mass
+        this.inverseMass = 1 / mass
 	}
 
 	applyImpulse(impulse) {
-		this.velocity.add(impulse)
+        const acceleration = Vector.clone(impulse).scale(this.inverseMass)
+		this.velocity.add(acceleration)
 	}
 
 	update() {
